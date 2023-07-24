2a12ccd637aae4619d9e1ee2b3de1317437e37c9
Pia <pia.pesl@gmail.com>
2023-07-24 13:47:20 +0200

    edit BackendApplicationTests

--- backend/src/test/java/de/neuefische/capstone/pia/backend/BackendApplicationTests.java
+++ backend/src/test/java/de/neuefische/capstone/pia/backend/BackendApplicationTests.java
@@ -1,5 +1,6 @@
 package de.neuefische.capstone.pia.backend;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BackendApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads()  {
+		Assertions.assertTrue(true);
 	}
 
 }
