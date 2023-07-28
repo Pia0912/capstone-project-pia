6af57efbf67b9a692ae8985e298fc79519f459b5
Pia <pia.pesl@gmail.com>
2023-07-28 19:01:47 +0200

    US 5 -frontend (add Activity model, edit hobby model, add ActivityItem, StarRating & HobbyDetail, add useActivities hook, some styling)
    add Backbutton for edit function

---
+++ frontend/src/components/StarRating.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import "../App.css";
+
+type StarRatingProps = {
+    initialRating: number;
+    onChange?: (newRating: number) => void;
+};
+
+const LOCAL_STORAGE_KEY = "starRating";
+
+export default function StarRating(props: StarRatingProps) {
+    const [rating, setRating] = useState<number>(() => {
+        const storedRating = localStorage.getItem(LOCAL_STORAGE_KEY);
+        return storedRating ? parseInt(storedRating, 10) : props.initialRating;
+    });
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, rating.toString());
+    }, [rating]);
+
+    const handleClick = (newRating: number) => {
+        setRating(newRating);
+        if (props.onChange) {
+            props.onChange(newRating);
+        }
+    };
+
+    return (
+        <div
+            className="ratingControl"
+        >
+            {[1, 2, 3, 4, 5].map((index) => (
+                <label
+                    key={index}
+                    style={{
+                        cursor: 'pointer',
+                        color: index <= rating ? '#ffbf00' : '#ccc',
+                    }}
+                    onClick={() => handleClick(index)}
+                >
+                    ★
+                </label>
+            ))}
+        </div>
+    );
+}
