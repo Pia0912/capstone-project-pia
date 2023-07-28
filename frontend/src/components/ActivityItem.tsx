76f146b10981e215dd47cf770dc32a573475cc4f
Pia <pia.pesl@gmail.com>
2023-07-28 11:45:18 +0200

    -frontend (grid styling)

--- frontend/src/components/ActivityItem.tsx
+++ frontend/src/components/ActivityItem.tsx
@@ -1,5 +1,8 @@
 import { Activity } from "../models";
 import { useLocation } from "react-router-dom";
+import StarRating from "./StarRating";
+import { useState } from "react";
+import { Grid } from "@mui/material";
 
 type Props = {
     activity: Activity;
@@ -9,19 +12,29 @@ export default function ActivityItem(props: Props) {
     const location = useLocation();
     const selectedColor = location.state?.selectedColor || "#f2f2f2";
 
+    const [lastSelectedRating, setLastSelectedRating] = useState(5);
+
+    const handleRatingChange = (newRating: number) => {
+        console.log("New rating:", newRating);
+        setLastSelectedRating(newRating);
+    };
+
     return (
-
-        <div className="flip-card" style={{ backgroundColor: selectedColor }}>
-            <div className="flip-card-inner">
-                <div className="flip-card-front" style={{ backgroundColor: selectedColor }}>
-                    <h3>{props.activity.name}</h3>
-                    <p>{props.activity.date}</p>
+        <>
+            <Grid item xs={6} sm={6} md={6} lg={6} container justifyContent="center" alignItems="center">
+                <div className="flip-card" style={{ backgroundColor: selectedColor }}>
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front" style={{ backgroundColor: selectedColor }}>
+                            <h3>{props.activity.name}</h3>
+                            <p>{props.activity.date}</p>
+                        </div>
+                        <div className="flip-card-back">
+                            <p>RATING: </p>
+                            <StarRating initialRating={lastSelectedRating} onChange={handleRatingChange} />
+                        </div>
+                    </div>
                 </div>
-                <div className="flip-card-back">
-
-                    <p>Rating: </p>
-                </div>
-            </div>
-        </div>
+            </Grid>
+        </>
     );
 }
